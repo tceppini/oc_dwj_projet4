e951ffc37e39c52e08771a651c20535f5e0e4b4d
tceppini <tcheppini@gmail.com>
2017-05-02 14:47:35 +0200

    Màj suivant Cdc P4
    modif nom tables

--- db/structure.sql
+++ db/structure.sql
@@ -1,26 +1,30 @@
-drop table if exists t_comment;
-drop table if exists t_user;
-drop table if exists t_article;
+drop table if exists P4_t_comment;
+drop table if exists P4_t_user;
+drop table if exists P4_t_billet;
 
-create table t_article (
-    art_id integer not null primary key auto_increment,
-    art_title varchar(100) not null,
-    art_content varchar(2000) not null
+create table P4_t_billet (
+    billet_id integer not null primary key auto_increment,
+    billet_title varchar(100) not null,
+    billet_content varchar(2000) not null,
+    billet_dateAjout date not null,
+    billet_dateModif date not null,
+    billet_nbComments int(255) NOT NULL
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
 
-create table t_user (
-    usr_id integer not null primary key auto_increment,
-    usr_name varchar(50) not null,
-    usr_password varchar(88) not null,
-    usr_salt varchar(23) not null,
-    usr_role varchar(50) not null 
-) engine=innodb character set utf8 collate utf8_unicode_ci;
-
-create table t_comment (
+create table P4_t_comment (
     com_id integer not null primary key auto_increment,
     com_content varchar(500) not null,
-    art_id integer not null,
-    usr_id integer not null,
-    constraint fk_com_art foreign key(art_id) references t_article(art_id),
-    constraint fk_com_usr foreign key(usr_id) references t_user(usr_id)
+    billet_id integer not null,
+    user_id integer not null,
+    com_date datetime not null,
+    constraint fk_com_billet foreign key(billet_id) references P4_t_billet(billet_id),
+    constraint fk_com_user foreign key(user_id) references P4_t_user(user_id)
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+
+create table P4_t_user (
+    user_id integer not null primary key auto_increment,
+    user_name varchar(50) not null,
+    user_password varchar(88) not null,
+    user_salt varchar(23) not null,
+    user_role varchar(50) not null 
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
