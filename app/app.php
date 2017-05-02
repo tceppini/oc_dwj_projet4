a988f2c9682951bf678a8cfe6e092c9f8dbe9c83
tceppini <tcheppini@gmail.com>
2017-05-02 14:35:16 +0200

    Màj suivant Cdc P4
    tests, page debug, insertion db -> OK

--- app/app.php
+++ app/app.php
@@ -10,11 +10,12 @@ ExceptionHandler::register();
 
 // Register service providers
 $app->register(new Silex\Provider\DoctrineServiceProvider());
+$app->register(new Silex\Provider\LocaleServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider());
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
-    'twig.options'    => array(
-        'cache' => __DIR__ . '/../var/cache/twig',
-        )
 ));
 $app['twig'] = $app->extend('twig', function(Twig_Environment $twig, $app) {
     $twig->addExtension(new Twig_Extensions_Extension_Text());
@@ -22,6 +23,11 @@ $app['twig'] = $app->extend('twig', function(Twig_Environment $twig, $app) {
 });
 $app->register(new Silex\Provider\AssetServiceProvider(), array(
     'assets.version' => 'v1'
+));
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../var/logs/writerblog.log',
+    'monolog.name' => 'writerblog',
+    'monolog.level' => $app['monolog.level']
 ));
 $app->register(new Silex\Provider\SessionServiceProvider());
 $app->register(new Silex\Provider\SecurityServiceProvider(), array(
@@ -32,7 +38,7 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
             'logout' => true,
             'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
             'users' => function () use ($app) {
-                return new MicroCMS\DAO\UserDAO($app['db']);
+                return new writerblog\DAO\UserDAO($app['db']);
             },
         ),
     ),
@@ -44,25 +50,18 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     ),
 ));
 $app->register(new Silex\Provider\FormServiceProvider());
-$app->register(new Silex\Provider\LocaleServiceProvider());
-$app->register(new Silex\Provider\TranslationServiceProvider());
-$app->register(new Silex\Provider\ValidatorServiceProvider());
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-    'monolog.logfile' => __DIR__.'/../var/logs/microcms.log',
-    'monolog.name' => 'MicroCMS',
-    'monolog.level' => $app['monolog.level']
-));
+
 
 // Register services
-$app['dao.article'] = function ($app) {
-    return new MicroCMS\DAO\ArticleDAO($app['db']);
+$app['dao.billet'] = function ($app) {
+    return new writerblog\DAO\BilletDAO($app['db']);
 };
 $app['dao.user'] = function ($app) {
-    return new MicroCMS\DAO\UserDAO($app['db']);
+    return new writerblog\DAO\UserDAO($app['db']);
 };
 $app['dao.comment'] = function ($app) {
-    $commentDAO = new MicroCMS\DAO\CommentDAO($app['db']);
-    $commentDAO->setArticleDAO($app['dao.article']);
+    $commentDAO = new writerblog\DAO\CommentDAO($app['db']);
+    $commentDAO->setBilletDAO($app['dao.billet']);
     $commentDAO->setUserDAO($app['dao.user']);
     return $commentDAO;
 };
@@ -80,12 +79,4 @@ $app->error(function (\Exception $e, Request $request, $code) use ($app) {
             $message = "Something went wrong.";
     }
     return $app['twig']->render('error.html.twig', array('message' => $message));
-});
-
-// Register JSON data decoder for JSON requests
-$app->before(function (Request $request) {
-    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
-        $data = json_decode($request->getContent(), true);
-        $request->request->replace(is_array($data) ? $data : array());
-    }
 });
