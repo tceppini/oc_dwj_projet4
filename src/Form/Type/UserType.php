499ff91ef6fd10db90666d6c7916b990e19cfe8f
tceppini <tcheppini@gmail.com>
2017-05-02 14:51:25 +0200

    Màj suivant Cdc P4

--- src/Form/Type/UserType.php
+++ src/Form/Type/UserType.php
@@ -1,34 +1,31 @@
 <?php
 
-namespace MicroCMS\Form\Type;
+namespace writerblog\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
-class UserType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('username', TextType::class)
-            ->add('password', RepeatedType::class, array(
-                'type'            => PasswordType::class,
-                 'invalid_message' => 'The password fields must match.',
-                 'options'         => array('required' => true),
-                 'first_options'   => array('label' => 'Password'),
-                 'second_options'  => array('label' => 'Repeat password'),
-             ))
-            ->add('role', ChoiceType::class, array(
-                'choices' => array('Admin' => 'ROLE_ADMIN', 'User' => 'ROLE_USER')
-            ));
+class UserType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('username', TextType::class);
+        $builder->add('password', RepeatedType::class, array(
+            'type'            => PasswordType::class,
+            'invalid_message' => 'Les champs du mot de passe doivent correspondre.',
+            'options'         => array('required' => true),
+            'first_options'   => array('label' => 'Password'),
+            'second_options'  => array('label' => 'Repeat password'),
+        ));
+        $builder->add('role', ChoiceType::class, array(
+            'choices' => array('Admin' => 'ROLE_ADMIN', 'User' => 'ROLE_USER')
+        ));
     }
 
-    public function getName()
-    {
+    public function getName() {
         return 'user';
     }
 }
