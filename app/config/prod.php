47b3fba4a79128a17f4dfe64be6af201303ad6dc
tceppini <tcheppini@gmail.com>
2017-05-11 10:15:58 +0200

    Màj info host OVH

--- app/config/prod.php
+++ app/config/prod.php
@@ -1,22 +1,15 @@
 <?php
 
-// Deployment on Heroku with ClearDB for MySQL
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
 // Doctrine (db)
 $app['db.options'] = array(
-    'driver'   => 'pdo_mysql',
-    'charset'  => 'utf8',
-    'host'     => "$server",
-    'port'     => '3306',
-    'dbname'   => "$db",
-    'user'     => "$username",
-    'password' => "$password",
-);
+	'driver'   => 'pdo_mysql',
+	'charset'  => 'utf8',
+	'host'     => 'tceppinitamcms.mysql.db',
+	'port'     => '3306',
+	'dbname'   => 'tceppinitamcms',
+	'user'     => 'tceppinitamcms',
+	'password' => 'Gtars57574',
+	);
 
-// define log parameters
+// define log level
 $app['monolog.level'] = 'WARNING';
